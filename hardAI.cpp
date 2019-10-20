1f06a6ab914a6505191733884c2f8484fb42c457
Joseph Pennington <jpennington222@ku.edu>
2019-10-20 14:54:11 -0500

    Update comments

--- hardAI.cpp
+++ hardAI.cpp
@@ -1,3 +1,7 @@
+/// File Name hardAI.cpp.
+/// Assignment EECS 448 Project 2.
+/// Brief Implements hard AI functions.
+
 #include "hardAI.h"
 #include <iostream>
 #include <stdlib.h>
@@ -37,7 +41,8 @@ hardAI::~hardAI() {
 
 void hardAI::printBoard()
 {
-  std::cout << "  A B C D E F G H"; ///COLUMN LABELS.
+  /// COLUMN LABELS.
+  std::cout << "  A B C D E F G H";
 
   for(int x = 0; x < m_rows; x++)
   {
@@ -53,7 +58,8 @@ void hardAI::printBoard()
 
 void hardAI::printAttackBoard()
 {
-  std::cout << "  A B C D E F G H"; ///COLUMN LABELS.
+  /// COLUMN LABELS.
+  std::cout << "  A B C D E F G H";
 
   for(int x = 0; x < m_rows; x++)
   {
@@ -76,12 +82,15 @@ void hardAI::printAttackBoard()
 
 void hardAI::incomingShot(std::string coords)
 {
-  int row = convertCoordinate(coords[0]);///Convert Coordinates to array indeces.
+  /// Convert Coordinates to array indeces.
+  int row = convertCoordinate(coords[0]);
   int col = convertCoordinate(coords[1]);
-  if(gameBoard[col][row] == '#')///Check if hits empty water, a miss.
+  /// Check if hits empty water, a miss.
+  if(gameBoard[col][row] == '#')
   {
     std::cout << "You missed.\n";
-    gameBoard[col][row] = 'M';///Mark the miss on the map.
+    /// Mark the miss on the map.
+    gameBoard[col][row] = 'M';
   }
   else if(gameBoard[col][row] == 'M' || gameBoard[col][row] == 'X')
   {
@@ -128,7 +137,7 @@ void hardAI::incomingShot(std::string coords)
 
 std::string hardAI::fireOnPlayer()
 {
-  ///returns a coordinate for AI to fire on.
+  /// returns a coordinate for AI to fire on.
   int row = 0;
   int col = 0;
   char rowArr[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
@@ -166,31 +175,37 @@ void hardAI::addShips(int numbShips)
     {
       xCoord = rand() % 8;
       yCoord = rand() % 8;
-      direction = rand() %4; //0-Up, 1-Right, 2-Down, 3-Left
-      if(gameBoard[xCoord][yCoord]=='#') //checks First Spot and going off board below
+      /// 0-Up, 1-Right, 2-Down, 3-Left.
+      direction = rand() %4;
+      /// checks First Spot and going off board below.
+      if(gameBoard[xCoord][yCoord]=='#')
       {
-          if(direction==0) //Up
+        /// Up.
+          if(direction==0)
           {
             if(xCoord-i>=0)
             {
               place=true;
             }
           }
-          else if(direction==1) //Right
+          /// Right.
+          else if(direction==1)
           {
             if(yCoord+i<=7)
             {
               place=true;
             }
           }
-          else if(direction==2) //Down
+          /// Down.
+          else if(direction==2)
           {
             if(xCoord+i<=7)
             {
               place=true;
             }
           }
-          else //Left
+          /// Left.
+          else
           {
             if(yCoord-i>=0)
             {
@@ -199,30 +214,35 @@ void hardAI::addShips(int numbShips)
           }
           if(place)
           {
-            for(int j=i;j>0;j--) //checks each position if it's water
+            /// checks each position if it's water.
+            for(int j=i;j>0;j--)
             {
-              if(direction==0) //up
+              /// up.
+              if(direction==0)
               {
                 if(gameBoard[xCoord-j][yCoord]!='#')
                 {
                   place=false;
                 }
               }
-              else if(direction==1) //right
+              /// right.
+              else if(direction==1)
               {
                 if(gameBoard[xCoord][yCoord+j]!='#')
                 {
                   place=false;
                 }
               }
-              else if(direction==2) //down
+              /// down.
+              else if(direction==2)
               {
                 if(gameBoard[xCoord+j][yCoord]!='#')
                 {
                   place=false;
                 }
               }
-              else //left
+              /// left.
+              else
               {
                 if(gameBoard[xCoord][yCoord-j]!='#')
                 {
@@ -231,99 +251,100 @@ void hardAI::addShips(int numbShips)
               }
             }
           }
-          if(place) //Places the Ships C,B,D,S,T
+          /// Places the Ships C,B,D,S,T.
+          if(place)
           {
             exit=true;
             for(int j=0; j<i;j++)
             {
               if(direction==0)
               {
-                if(i==5)//Ship length 5
+                if(i==5)
                 {
                   gameBoard[xCoord-j][yCoord]='C';
                 }
-                if(i==4)//Ship length 5
+                if(i==4)
                 {
                   gameBoard[xCoord-j][yCoord]='B';
                 }
-                if(i==3)//Ship length 5
+                if(i==3)
                 {
                   gameBoard[xCoord-j][yCoord]='D';
                 }
-                if(i==2)//Ship length 5
+                if(i==2)
                 {
                   gameBoard[xCoord-j][yCoord]='S';
                 }
-                if(i==1)//Ship length 5
+                if(i==1)
                 {
                   gameBoard[xCoord-j][yCoord]='T';
                 }
               }
               if(direction==1)
               {
-                if(i==5)//Ship length 5
+                if(i==5)
                 {
                   gameBoard[xCoord][yCoord+j]='C';
                 }
-                if(i==4)//Ship length 5
+                if(i==4)
                 {
                   gameBoard[xCoord][yCoord+j]='B';
                 }
-                if(i==3)//Ship length 5
+                if(i==3)
                 {
                   gameBoard[xCoord][yCoord+j]='D';
                 }
-                if(i==2)//Ship length 5
+                if(i==2)
                 {
                   gameBoard[xCoord][yCoord+j]='S';
                 }
-                if(i==1)//Ship length 5
+                if(i==1)
                 {
                   gameBoard[xCoord][yCoord+j]='T';
                 }
               }
               if(direction==2)
               {
-                if(i==5)//Ship length 5
+                if(i==5)
                 {
                   gameBoard[xCoord+j][yCoord]='C';
                 }
-                if(i==4)//Ship length 5
+                if(i==4)
                 {
                   gameBoard[xCoord+j][yCoord]='B';
                 }
-                if(i==3)//Ship length 5
+                if(i==3)
                 {
                   gameBoard[xCoord+j][yCoord]='D';
                 }
-                if(i==2)//Ship length 5
+                if(i==2)
                 {
                   gameBoard[xCoord+j][yCoord]='S';
                 }
-                if(i==1)//Ship length 5
+                if(i==1)
                 {
                   gameBoard[xCoord+j][yCoord]='T';
                 }
               }
               if(direction==3)
               {
-                if(i==5)//Ship length 5
+                if(i==5)
                 {
                   gameBoard[xCoord][yCoord-j]='C';
                 }
-                if(i==4)//Ship length 5
+                if(i==4)
                 {
                   gameBoard[xCoord][yCoord-j]='B';
                 }
-                if(i==3)//Ship length 5
+                if(i==3)
                 {
                   gameBoard[xCoord][yCoord-j]='D';
                 }
-                if(i==2)//Ship length 5
+                if(i==2)
                 {
                   gameBoard[xCoord][yCoord-j]='S';
                 }
-                if(i==1)//Ship length 5
+                if(i==1)
                 {
                   gameBoard[xCoord][yCoord-j]='T';
                 }
@@ -337,16 +358,19 @@ void hardAI::addShips(int numbShips)
 
 bool hardAI::validCoordinates(std::string& coords)
 {
-  if(coords.length() != 2)///Checks coordinate set is exactly a column and row with length.
+  /// Checks coordinate set is exactly a column and row with length.
+  if(coords.length() != 2)
   {
     return false;
   }
-  if(coords[0] == 'a' || coords[0] == 'b' || coords[0] == 'c' || coords[0] == 'd' || coords[0] == 'e' || coords[0] == 'f' || coords[0] == 'g' || coords[0] == 'h')///Uppercases first coordinate if necessary.
+  /// Uppercases first coordinate if necessary.
+  if(coords[0] == 'a' || coords[0] == 'b' || coords[0] == 'c' || coords[0] == 'd' || coords[0] == 'e' || coords[0] == 'f' || coords[0] == 'g' || coords[0] == 'h')
   {
     coords[0] = toupper(coords[0]);
   }
   if((coords[0] == 'A' || coords[0] == 'B' || coords[0] == 'C' || coords[0] == 'D' || coords[0] == 'E' || coords[0] == 'F' || coords[0] == 'G' || coords[0] == 'H') &&
-  (coords[1] == '1' || coords[1] == '2' || coords[1] == '3' || coords[1] == '4' || coords[1] == '5' || coords[1] == '6' || coords[1] == '7' || coords[1] == '8'))///Checks first is A-H, second is 1-8.
+  /// Checks first is A-H, second is 1-8.
+  (coords[1] == '1' || coords[1] == '2' || coords[1] == '3' || coords[1] == '4' || coords[1] == '5' || coords[1] == '6' || coords[1] == '7' || coords[1] == '8'))
   {
     return true;
   }
@@ -371,7 +395,8 @@ int hardAI::convertCoordinate(char coord)
 
 bool hardAI::gameOver()
 {
-  bool over = true;///Iterates through map, simply setting over to false if it sees a ship marker.
+  /// Iterates through map, simply setting over to false if it sees a ship marker.
+  bool over = true;
   for(int x = 0; x < m_rows; x++)
   {
     for(int y = 0; y < m_cols; y++)
@@ -387,7 +412,8 @@ bool hardAI::gameOver()
 
 bool hardAI::isShipSunk(char shipType)
 {
-  bool isSunk = true;///Iterates through map, simply setting isSunk to false if it sees the specified ship marker.
+  /// Iterates through map, simply setting isSunk to false if it sees the specified ship marker.
+  bool isSunk = true;
   for(int x = 0; x < m_rows; x++)
   {
     for(int y = 0; y < m_cols; y++)
